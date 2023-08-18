216f30de2e6e94a09dff0e8a5623bb6e89fb8869
Eduarda Waechter <d.wchtr@gmail.com>
2023-08-18 17:23:17 -0300

    bad start

--- kit_games/advsearch/your_agent/mcts.py
+++ kit_games/advsearch/your_agent/mcts.py
@@ -1,4 +1,6 @@
+from math import log, sqrt
 import random
+from time import time
 from typing import Tuple
 
 # Voce pode criar funcoes auxiliares neste arquivo
@@ -7,7 +9,36 @@ from typing import Tuple
 # Nao esqueca de renomear 'your_agent' com o nome
 # do seu agente.
 
+C = 1
+MAX_TIME = 5
+class Nodo:
+    def __init__(self, state, parent=None, move=None):
+        self.state = state
+        self.parent = parent
+        self.move = move
 
+        self.children = []
+        self.wins = 0
+        self.visits = 0
+        self.possible_moves = state.legal_moves()
+
+    def ucb_select(self):
+        selection = sorted(self.children, key = lambda c: c.wins/c.visits + 2*C*sqrt(2*log(self.visits)/c.visits))
+        return selection
+    
+    def add_child(self, move, state):
+        n = Nodo(state, self, move)
+        self.possible_moves.remove(move)
+        self.children.append(n)
+
+        return n
+
+def backpropagation(nodo: Nodo, result):
+    while nodo != None:
+        nodo.visits += 1
+        nodo.wins += result
+        nodo = nodo.parent
+    
 def make_move(state) -> Tuple[int, int]:
     """
     Returns a move for the given game state. 
@@ -21,6 +52,31 @@ def make_move(state) -> Tuple[int, int]:
     # o codigo abaixo retorna uma jogada ilegal
     # Remova-o e coloque a sua implementacao do MCTS
 
-    return (-1, -1)
+    start_time = time()
+    raiz = Nodo(state)
+
+    while time() - start_time < MAX_TIME:
+        nodo = raiz
+
+        while nodo.possible_moves == [] and nodo.children != []:
+            nodo = nodo.ucb_select()
+
+        while not nodo.state.is_terminal():
+            move = random.choice(nodo.possible_moves)
+            new_state = nodo.state.copy().next_state(move)
+            next_node = Nodo(new_state, nodo, move)
+            nodo = nodo.add_child(next_node, move, new_state)
+
+        winner = nodo.state.winner()
+        result = 0
+        if winner == state.player:
+            result = 1
+        elif winner == state.get_board().opponent(state.player):
+            result = -1
+
+        backpropagation(nodo, result)
+
+    return nodo.state.move
+
 
 
--- kit_games/advsearch/your_agent/minimax.py
+++ kit_games/advsearch/your_agent/minimax.py
@@ -1,7 +1,17 @@
 import random
 from typing import Tuple, Callable
 
-
+def alpha_beta_pruning(state, depth:int, alpha:float, beta:float, eval_func:Callable) -> float:
+    
+    if depth == 0 or state.is_terminal():
+        return eval_func(state, state.player)
+    
+    is_max = False
+    if state.player == 'W':
+        is_max = True
+    
+    if is_max:
+        value = 
 
 def minimax_move(state, max_depth:int, eval_func:Callable) -> Tuple[int, int]:
     """
@@ -13,4 +23,14 @@ def minimax_move(state, max_depth:int, eval_func:Callable) -> Tuple[int, int]:
                     and should return a float value representing the utility of the state for the player.
     :return: (int, int) tuple with x, y coordinates of the move (remember: 0 is the first row/column)
     """
+    moves = state.legal_moves()
+
+    if len(moves) == 0:
+        return (-1, -1)
+    
+    best_value = 0
+    best_move = None
+
+    for move in moves:
+
     raise NotImplementedError()

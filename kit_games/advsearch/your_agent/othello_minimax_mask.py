f9a8df75e13586f434dd8b14e169863519dec09c
Eduarda Waechter <d.wchtr@gmail.com>
2023-08-22 21:27:50 -0300

    fix

--- kit_games/advsearch/your_agent/othello_minimax_mask.py
+++ kit_games/advsearch/your_agent/othello_minimax_mask.py
@@ -38,7 +38,7 @@ def make_move(state) -> Tuple[int, int]:
     # Remova-o e coloque uma chamada para o minimax_move (que vc implementara' no modulo minimax).
     # A chamada a minimax_move deve receber sua funcao evaluate como parametro.
 
-    return random.choice([(2, 3), (4, 5), (5, 4), (3, 2)])
+    return minimax_move(state, 4, evaluate_mask)
 
 
 def evaluate_mask(state, player: str) -> float:
